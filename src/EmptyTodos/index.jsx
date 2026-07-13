6b0647ef0c9cd4e85530e8275b75c41daafcbb26
Esteban Mente <menteesteban5@gmail.com>
2026-07-13 10:31:40 -0600

    nueva actualizacion

--- src/EmptyTodos/index.jsx
+++ src/EmptyTodos/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import './index.css'
 
 const EmptyTodos = () => {
     return (
-        <p>nose jaja</p>
+        <div className="todo-not-found">
+            <div className="matrix-bg">
+                {/* Capas para simular las líneas de código binario */}
+                <div className="binary-line">01101001 01101110 01110100 01100101</div>
+                <div className="binary-line delay-1">10010110 11100111 01010101 00110011</div>
+                <div className="binary-line delay-2">01110100 01101111 01100100 01101111</div>
+                <div className="binary-line delay-3">11010011 00110111 11100101 01101001</div>
+            </div>
+
+            <div className="matrix-content">
+                <div className="matrix-icon">
+                    <span className="scanner-line"></span>
+                    🔍
+                </div>
+                <h3 className="matrix-title">TODO NO ENCONTRADO</h3>
+                <p className="matrix-text">Crea uno nuevo presionando el botón '+' de abajo</p>
+            </div>
+        </div>
     );
 }
 
-export {EmptyTodos};
+export { EmptyTodos };
